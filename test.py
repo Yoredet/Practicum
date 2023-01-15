357db1f51f30f2a1171e7c32d5c608564317c1d0
Yoredet <122642842+Yoredet@users.noreply.github.com>
2023-01-15 21:16:16 +0400

    Pro mishu

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 print('Hello World!')
+n, m = map(int, input().split())
+a = []
+b = []
+count = 0
+for i in range(n):
+    a.append(list(input()))
+input()
+for i in range(n):
+    b.append(list(input()))
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == b[i][j]:
+            count += 1
+print(count)
+
